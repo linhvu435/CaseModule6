7142c693fe078dd4dcde10d3df20b4ea7584ecd9
sangpk <dinhsangcnc@gmail.com>
2023-01-10 10:48:37 +0700

    update

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -9,28 +9,37 @@ import {Router} from "@angular/router";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  checkUser! : any
   constructor(private loginService: LoginService, private router: Router) {
   }
 
   ngOnInit(): void {
+
 
   }
 
   registerForm = new FormGroup({
     email : new FormControl('',[Validators.email,Validators.required]),
     // name : new FormControl("",Validators.required),
-    phoneNumber : new FormControl("",Validators.required),
+    phoneNumber : new FormControl("",Validators.maxLength(12)),
     username : new FormControl("", Validators.maxLength(20)),
     password : new FormControl("",[Validators.minLength(3), Validators.maxLength(20)]),
     confirmPassword : new FormControl("",[Validators.minLength(3), Validators.maxLength(20)]),
-    img : new FormControl("",Validators.required)
+    adress : new FormControl("",Validators.required),
+    gender : new FormControl(""),
+
   })
 
   register() {
-    this.loginService.register(this.registerForm.value).subscribe((data) => {
-      this.router.navigate(["/login"]);
-    })
+    if (!this.registerForm.valid || this.registerForm.get("confirmPassword")?.value != this.registerForm.get("password")?.value){
+       alert("đăng kí không được các ô không được để trống")
+    }else {
+      this.loginService.register(this.registerForm.value).subscribe((data) =>  {
+        alert("đăng kí thành công")
+        this.router.navigate(["/login"]);
+
+      })
+    }
   }
 
 }
